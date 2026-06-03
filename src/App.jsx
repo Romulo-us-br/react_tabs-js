27d89d30f9d33034f6e654a700138dd1addccec4
Romulo Brasil <romulop.brasil25@gmail.com>
2026-06-02 22:20:55 -0300

    test 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react'; // Adicionado o useState aqui
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import tabsFromServer from './tabsFromServer.json'
+import tabsFromServer from './tabsFromServer.json';
+import { Tabs } from './components/Tabs';
 
-
+function getPreparedTabs(tabList) {
+  return [...tabList]; // Deixando a cópia simples como você fez
+}
 
 export const App = () => {
-
   const visibleTabs = getPreparedTabs(tabsFromServer);
+
+  // 1. Criando o estado para controlar qual ID de aba está ativo
+  const [activeTabId, setActiveTabId] = useState(visibleTabs[0]?.id || 0);
+
+  // 2. Encontrando a aba ativa atual para conseguir pegar o título dela no <h1>
+  const currentActiveTab = visibleTabs.find(tab => tab.id === activeTabId) || visibleTabs[0];
+
   return (
+    <main className="main">
+      <div className="section">
+        {/* Requisito 8: Mostrar o título da aba ativa no h1 do App */}
+        {currentActiveTab && (
+          <h1 className="title">Selected tab is {currentActiveTab.title}</h1>
+        )}
 
-    <div className="section">
-      <h1 className="title">Selected tab is Tab 1</h1>
-
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            <li className="is-active" data-cy="Tab">
-              <a href="#tab-1" data-cy="TabLink">
-                Tab 1
-              </a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-2" data-cy="TabLink">
-                Tab 2
-              </a>
-            </li>
-
-            <li data-cy="Tab">
-              <a href="#tab-3" data-cy="TabLink">
-                Tab 3
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Some text 1
-        </div>
+        {/* Passando as props com os nomes exatos exigidos pelo exercício */}
+        <Tabs 
+          tabs={visibleTabs} 
+          activeTabId={activeTabId} 
+          onTabSelected={(id) => setActiveTabId(id)} 
+        />
       </div>
-    </div>
-  )
-}
+    </main>
+  );
+};
